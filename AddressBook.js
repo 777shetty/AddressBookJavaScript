b6f8836e9e74f20b837d37fce66273ec4e13ead1
777shetty <shetty.chetu7@gmail.com>
2021-10-09 11:07:34 +0530

    ADDED:Method to print contacts for addressbook

---
+++ AddressBook.js
@@ -0,0 +1,32 @@
+class Contact{
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phone;
+    email;
+
+    constructor(...params)
+    {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phone = params[6];
+        this.email = params[7];
+    }
+
+    toString()
+    {
+        return "FirstName = "+this.firstName+", LastName = "+this.lastName+", Address = "+this.address+
+                ", City = "+this.city+", State = "+this.state+", Zip = "+this.zip+", Phone = "+this.phone+", Email = "+this.email;
+    }
+}
+
+let contact = new Contact("Rahul","Pandey","2nd cross, electronic city","Bangalore","Karnataka",560100,9876432387,"rahulp@yahoo.com");
+console.log(contact.toString());
